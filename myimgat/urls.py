0387873a673d1cb4c454c668ee9db4444d087287
Fabio Miranda Costa <fabiomcosta@gmail.com>
2011-07-29 23:18:48 -0300

    favicon route

--- myimgat/urls.py
+++ myimgat/urls.py
@@ -1,5 +1,8 @@
+
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.views.generic.simple import redirect_to
 
 admin.autodiscover()
 
@@ -7,6 +10,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myimgat.views.home', name='home'),
     url(r'^$', include('wall.urls')),
+    url('^favicon.ico$', redirect_to, {'url': '%s/favicon.ico' % settings.STATIC_URL}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
