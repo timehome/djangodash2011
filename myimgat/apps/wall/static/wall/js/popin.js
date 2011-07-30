84d723da15170a1f8505eee14fe42fb099c608f5
Fabio Miranda Costa <fabiomcosta@gmail.com>
2011-07-30 14:05:07 -0300

    adding hide when clicking on overlay, and the overlay itself :S

--- myimgat/apps/wall/static/wall/js/popin.js
+++ myimgat/apps/wall/static/wall/js/popin.js
@@ -5,28 +5,44 @@
 
         initialize: function(element) {
             this.element = $(element);
-            this.tabs = this.element.getChildren();
             this.element.fade('hide');
+            this.tabs = this.element.getChildren();
+            this.createOverlay();
         },
 
-        hideOthers: function() {
-            this.tabs.removeClass('show');
+        createOverlay: function() {
+            this.overlay = new Element('div', {
+                'class': 'overlay',
+                events: {
+                    click: this.hide.bind(this)
+                }
+            });
+            this.overlay.fade('hide');
+            $$('body').grab(this.overlay);
         },
 
         show: function(tab) {
-            this.element.fade('in');
             if (tab) {
                 this.showTab(tab);
             }
+            this.element.fade('in');
+            this.overlay.fade('in');
         },
 
         hide: function() {
             this.element.fade('out');
+            this.overlay.fade('out');
+        },
+
+        hideTabs: function() {
+            this.tabs.removeClass('show');
         },
 
         showTab: function(tab) {
+            this.hideTabs();
             this.element.getElement('> .'+ tab).addClass('show');
         }
+
     });
 
 }(this, document.id));
