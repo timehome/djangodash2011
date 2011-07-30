3494ebdb159aef4a3692988755c56f6fc259c645
Rafael Caricio <rafael.jacinto@gmail.com>
2011-07-29 23:01:55 -0300

    Updating the wsgi config.

--- myimgat/deploy/wsgi.py
+++ myimgat/deploy/wsgi.py
@@ -1,7 +1,7 @@
 import os, sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir, os.pardir)))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')))
 
 from django.core.handlers.wsgi import WSGIHandler
 os.environ["DJANGO_SETTINGS_MODULE"] = "myproject.settings"
