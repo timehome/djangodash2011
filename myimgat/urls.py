2cb0be78fb1cce04b8ed5e404685b704fb6dafd9
Rafael Caricio <rafael.jacinto@gmail.com>
2011-07-29 23:29:04 -0300

    updating urls.

--- myimgat/urls.py
+++ myimgat/urls.py
@@ -7,14 +7,9 @@ from django.views.generic.simple import redirect_to
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myimgat.views.home', name='home'),
     url(r'^$', include('wall.urls')),
     url('^favicon.ico$', redirect_to, {'url': '%s/favicon.ico' % settings.STATIC_URL}),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    # admin
     url(r'^admin/', include(admin.site.urls)),
 )
