c29cf2ee0a54092bf8c46bd2841e031a6a7fa9de
Fabio Miranda Costa <fabiomcosta@gmail.com>
2011-07-31 16:04:30 -0300

    hidding the image when pressing share

--- myimgat/apps/wall/static/wall/js/crop-popin.js
+++ myimgat/apps/wall/static/wall/js/crop-popin.js
@@ -5,6 +5,7 @@
 
         initialize: function(){
             this.parent.apply(this, arguments);
+            this.selectElements();
             this.bindEvents();
             this.createRequest();
         },
@@ -15,16 +16,20 @@
             });
         },
 
-        bindEvents: function() {
+        selectElements: function() {
             this.shareButton = this.element.getElement('.share-button');
             this.token = this.element.getElement('[name="csrfmiddlewaretoken"]');
-            this.shareButton.addEvent('click', this.share.bind(this));
             this.shareArea = this.element.getElement('div.share-details');
             this.croppedLink = this.element.getElement('a.url');
             this.embedURL = this.element.getElement('span.url');
             this.embedTitle = this.element.getElement('span.title');
             this.embedComments = this.element.getElement('span.comment-page');
             this.socials = this.element.getElement('div.socials');
+            this.cropContainer = this.element.getElement('div.crop-image-container');
+        },
+
+        bindEvents: function() {
+            this.shareButton.addEvent('click', this.share.bind(this));
         },
 
         share: function(e) {
@@ -35,6 +40,7 @@
             data[this.token.get('name')] = this.token.get('value');
             this.request.addEvent('success', function(url) {
                 var shareUrl = 'http://myimg.at/shared_photo/' + self.image.id;
+                self.cropContainer.morph({height: 0});
                 self.croppedLink.set('href', url);
                 self.croppedLink.set('text', self.image.title);
                 self.embedURL.set('text', url);
