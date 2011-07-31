d8eb5ce09b2ffcf321b0f7434edc3766a1fe5206
Bernardo Heynemann <heynemann@gmail.com>
2011-07-31 14:48:58 -0300

    Popin

--- myimgat/apps/wall/static/wall/js/crop-popin.js
+++ myimgat/apps/wall/static/wall/js/crop-popin.js
@@ -10,7 +10,7 @@
         },
 
         createRequest: function(){
-            this.request = new Request.JSON({
+            this.request = new Request({
                 url: settings.urls.shorten
             });
         },
@@ -19,15 +19,25 @@
             this.shareButton = this.element.getElement('.share-button');
             this.token = this.element.getElement('[name="csrfmiddlewaretoken"]');
             this.shareButton.addEvent('click', this.share.bind(this));
+            this.croppedLink = this.element.getElement('a.url');
+            this.embedURL = this.element.getElement('span.url');
+            this.embedTitle = this.element.getElement('span.title');
+            this.embedComments = this.element.getElement('span.comment-page');
         },
 
         share: function(e) {
+            var self = this;
             e.preventDefault();
             var data = Object.clone(this.shareButton.retrieve('crop-info'));
             data["id"] = this.image.id;
             data[this.token.get('name')] = this.token.get('value');
             this.request.addEvent('success', function(url) {
-                console.log('url');
+                self.croppedLink.set('href', url);
+                self.croppedLink.set('text', self.image.title);
+                self.embedURL.set('text', url);
+                self.embedTitle.set('text', self.image.title);
+                self.embedComments.set('text', 'http://myimg.at/shared_photo/' + self.image.id);
+
             }).post(data);
         },
 
