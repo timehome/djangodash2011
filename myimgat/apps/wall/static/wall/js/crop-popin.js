fcbb47fab2a539f35b56985b47fb21ae638937ea
Bernardo Heynemann <heynemann@gmail.com>
2011-07-31 16:02:16 -0300

    All shares

--- myimgat/apps/wall/static/wall/js/crop-popin.js
+++ myimgat/apps/wall/static/wall/js/crop-popin.js
@@ -45,7 +45,7 @@
                 var twitter = '<div class="social-item"><a target="_blank" title="tweet this image" href="http://twitter.com/share?url=' + shareUrl + '&via=myimgat&text=' + self.image.title + '" class="twitter-share-button"><img src="/static/wall/img/tweetn.png" alt="tweet this image" /></a></div>';
                 var gplus = '<div class="social-item"><g:plusone href="url"></g:plusone></div>';
                 var fbLike = '<div class="social-item"><iframe src="http://www.facebook.com/plugins/like.php?app_id=102452659856143&amp;href=' + shareUrl + '&amp;send=false&amp;layout=standard&amp;width=450&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font=arial&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:300px; height:80px; margin-left:10px;" allowTransparency="true"></iframe></div>';
-                var fbSend = '<div class="social-item"><div id="fb-root"></div><fb:send href="' + shareUrl + '" font="arial"></fb:send></div>';
+                var fbSend = '<div class="social-item"><div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script><fb:send href="' + shareUrl + '" font="arial"></fb:send></div>';
 
                 self.socials.set('html', twitter + gplus + fbLike + fbSend);
 
