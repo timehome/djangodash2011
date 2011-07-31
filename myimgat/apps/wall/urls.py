74c54fee9d1528b09896c708a5455571b698f745
Bernardo Heynemann <heynemann@gmail.com>
2011-07-31 02:19:20 -0300

    Save cropped photo

--- myimgat/apps/wall/urls.py
+++ myimgat/apps/wall/urls.py
@@ -4,7 +4,7 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.generic.list_detail import object_detail
 
-from wall.views import index, albums
+from wall.views import index, albums, save_cropped_photo
 from wall.models import CroppedPhoto, Photo
 
 urlpatterns = patterns('',
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     url('^$', index),
 
     url('^api/(?P<username>[\w.]+).(?P<extension>(json|jsonp))$', albums),
-    url('^api/shorten/?$', albums),
+    url('^api/shorten/?$', save_cropped_photo),
 
     url('^(?P<object_id>\d+)[.](?:jpe?g|gif|png)$', object_detail, {
             'queryset': CroppedPhoto.objects.all(),
